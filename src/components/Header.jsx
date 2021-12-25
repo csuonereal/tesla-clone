f1d00480b852830ded27b83097115f31a6f193c7
csuonereal <68502410+csuonereal@users.noreply.github.com>
2021-12-25 16:57:14 +0300

    part 2 is done.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
-
+import { FaBars, FaTimes } from "react-icons/fa";
 const Header = (props) => {
+  const [burgerStatus, setBurgerStatus] = useState(false);
   return (
     <Container>
       <a>
         <img src="/images/logo.svg" alt="logo" />
       </a>
       <Menu>
-        <p>
-          <a href="#">Model S</a>
-        </p>
-        <p>
-          <a href="#">Model 3</a>
-        </p>
-        <p>
-          <a href="#">Model X</a>
-        </p>
-        <p>
-          <a href="#">Model Y</a>
-        </p>
+        <a href="#">Model S</a>
+        <a href="#">Model 3</a>
+        <a href="#">Model X</a>
+        <a href="#">Model Y</a>
       </Menu>
+      <RightMenu>
+        <a href="#">Shop</a>
+        <a href="#">Tesla Account</a>
+        <CustomMenu onClick={() => setBurgerStatus(true)} />
+      </RightMenu>
+      <BurgerNav show={burgerStatus}>
+        <CloseWrapper>
+          <CustomClose onClick={() => setBurgerStatus(false)} />
+        </CloseWrapper>
+        <li>
+          <a href="#">Existing Inventory</a>
+        </li>
+        <li>
+          <a href="#">Used Inventory</a>
+        </li>
+        <li>
+          <a href="#">Trade-in</a>
+        </li>
+        <li>
+          <a href="#">Cybertruck</a>
+        </li>
+        <li>
+          <a href="#">Roadaster</a>
+        </li>
+        <li>
+          <a href="#">Existing Inventory</a>
+        </li>
+      </BurgerNav>
     </Container>
   );
 };
@@ -32,15 +53,73 @@ const Container = styled.div`
   position: fixed;
   display: flex;
   align-items: center;
+  justify-content: space-between;
   padding: 0 20px;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 1;
 `;
 
 const Menu = styled.div`
   display: flex;
   align-items: center;
-  p {
+  justify-content: center;
+  flex: 1;
+  a {
     font-weight: 600;
     text-transform: uppercase;
     padding: 0 10px; //zero for bottom and top 10px for left and right
   }
+
+  @media (max-width: 768px) {
+    display: none;
+  }
+`;
+
+const RightMenu = styled.div`
+  display: flex;
+  align-items: center;
+  a {
+    font-weight: 600;
+    text-transform: uppercase;
+    padding: 0 10px; //zero for bottom and top 10px for left and right
+  }
+`;
+
+const CustomMenu = styled(FaBars)`
+  cursor: pointer;
+`;
+
+const BurgerNav = styled.div`
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  background-color: white;
+  width: 300px;
+  z-index: 100;
+  list-style: none;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  text-align: start;
+  transform: ${(n) => (n.show ? "translateX(0)" : "translateX(100%)")};
+  transition: transform 0.2s ease-in;
+  li {
+    padding: 15px 0;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+    a {
+      font-weight: 600;
+    }
+  }
+`;
+
+const CustomClose = styled(FaTimes)`
+  cursor: pointer;
+`;
+
+const CloseWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
 `;
